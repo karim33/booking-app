2a5327ee2ec15ccf0f076c5e2d8754fa2239beb5
osman abate <abateo@uw.edu>
2017-10-16 18:33:38 -0700

    added mySQL

--- booking_app.py
+++ booking_app.py
@@ -1,7 +1,31 @@
 from flask import Flask, render_template, request, redirect, url_for, session, flash
+from flask_sqlalchemy import SQLAlchemy
+import os
 
 app = Flask(__name__)
 app.config['DEBUG'] = True
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', 'mysql+pymysql://Booking-app:osman123@localhost:8889/Booking-app')
+app.config['SQLALCHEMY_ECHO'] = True
+db = SQLAlchemy(app)
+#app.secret_key = 'welloethio'
+
+
+class User(db.Model):
+
+    id = db.Column(db.Integer, primary_key=True)
+    FirstName = db.Column(db.String(120))
+    LastName = db.Column(db.String(120))
+    Email = db.Column(db.String(120), unique=True)
+    Address = db.Column(db.String(120))
+    ServiceType = db.Column(db.String(220))
+    #posts = db.relationship('Blog', backref='owner')
+
+    def __init__(self, FirstName, LastName, Email, Address, ServiceType):
+        self.FirstName = FirstName
+        self.LastName = LastName
+        self.Email = Email
+        self.Address = Address
+        self.ServiceType = ServiceType
 
 @app.errorhandler(404)
 def page_not_found(e):
@@ -31,4 +55,6 @@ def about():
 def form():
     return render_template('form.html')
 
-app.run()
+
+if __name__ == '__booking_app__':
+	app.run()
